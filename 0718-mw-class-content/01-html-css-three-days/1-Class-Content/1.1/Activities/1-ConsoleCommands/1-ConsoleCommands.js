a7b22bf169a196fdfed3a90348c59750de44cc21
Brian Doyal <brian.doyal@gmail.com>
2016-07-20 12:38:39 -0500

    Add slide show, images, console activity

---
+++ 0718-mw-class-content/01-html-css-three-days/1-Class-Content/1.1/Activities/1-ConsoleCommands/1-ConsoleCommands.js
@@ -0,0 +1,56 @@
+/** 
+ * Bash / Terminal Commands
+ *
+ * These are the basic commands for navigating directories 
+ * in a Terminal or Git Bash window.
+ *
+ **/
+
+// Present Working Directory
+pwd
+
+
+// List files and directories
+ls
+
+// List with -al flag (shows all files/directories in long list format)
+ls -al
+
+
+// Change Directory
+cd 
+
+// Change to Root (Home) Directory
+cd ~
+
+// Move to One Directory Up
+cd ..
+
+
+// Make New File
+touch [name of file to create]
+
+// Make New Folder
+mkdir [name of directory to creat]
+
+
+
+// Delete file
+rm [name of file to remove]
+
+// Delete folder
+rm -r [name of directory to remove]
+
+
+
+// Open file or folder (Mac only)
+open [name of file]
+
+// Open all files and folder in current directory (Man only)
+open .
+
+// Open file or folder (PC only)
+explorer
+
+// Open all files and folder in current directory (PC Only)
+explorer .
